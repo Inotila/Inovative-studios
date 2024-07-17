4ad411a6e56f3e668cc2b7f6006ad9b93381f011
inoati1991@gmail.com <inoati1991@gmail.com>
2024-07-17 13:12:43 +0200

    Add contentful.js to .gitignore

--- backend/music/contentful_connector/contentful.js
+++
@@ -1,9 +0,0 @@
-const contentful = require('contentful');
-const client = contentful.createClient({
-    space: '47cj9gyw95uk',
-    accessToken: 'PqA6OvMoFavMCh-jdpT-O_jmLYisWv7yC3BkrcrsI6k',
-});
-
-module.exports = {
-    client
-};
