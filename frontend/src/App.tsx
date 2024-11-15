c2f08f99412fbb9dfb382743302a4c65a29fc421
inoati1991@gmail.com <inoati1991@gmail.com>
2024-11-15 17:57:03 +0100

    Update case footer to Footer to meet case sesnitivity requirements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/home/HomePage';  // Keeping the HomePage component
 import Navbar from './components/navbar/Navbar';  
-import Footer from './components/footer/footer';
+import Footer from './components/footer/Footer';
 
 const App: React.FC = () => {
   return (
