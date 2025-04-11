ae4e2bd8eb5d1ce51698306a5f87bc6c89040c1b
inoati1991@gmail.com <inoati1991@gmail.com>
2025-04-11 03:12:09 +0200

    Added style to buttons and refactored buttons to be links instead

--- frontend/src/components/home/HomePage.tsx
+++ frontend/src/components/home/HomePage.tsx
@@ -6,59 +6,63 @@ import './assets/css/HomePage.css';
 const HomePage: React.FC = () => {
   const imagePath = '/images/home/';
   return (
-      <div className="container text-center">
-        <div className="row">
-          <div className="col">
-            <h1>Inovative Studios</h1>
-          </div>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <h1>Inovative Studios</h1>
         </div>
-        <div className="row">
-          <div className="col">
-            <p>Welcome to Inovative Studios
+      </div>
+      <div className="row">
+        <div className="col">
+          <p>Welcome to Inovative Studios
             <br />
-          We make stuff and have fun a long the way!</p>
+            We make stuff and have fun a long the way!</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="card home-card">
+            <Link to="/" className='home-img-links-container'>
+              <img className="home-img-links card-img-top" src={imagePath + "services.png"}
+                alt="#" aria-label="an image link leads to details about the art work" />
+            </Link>
+            <Link to="#" className="btn main-btn mt-2">
+              <h5 className="card-title">Services</h5>
+            </Link>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="card home-card">
-                <Link to="/" className='home-img-links-container'>
-                  <img className="home-img-links card-img-top" src={imagePath + "services.png"}
-                        alt="#" aria-label="an image link leads to details about the art work"/>
-                </Link>
-              <button className='home-link-button'> services</button>
-            </div>
-          </div>
-          <div className="col">
+        <div className="col">
           <div className="card">
-                <Link to="/entertainment" className='home-img-links-container'>
-                  <img className="home-img-links card-img-top" src={imagePath + "entertainment.png"}
-                        alt="#" aria-label="an image link leads to details about the art work"/>
-                </Link>
-                <Link to="/entertainment">
-                  <button className="home-link-button"> Media Center</button>
-                </Link>
-            </div>
-          </div>
-          <div className="col">
-          <div className="card">
-                <Link to="/" className='home-img-links-container'>
-                  <img className="home-img-links card-img-top" src={imagePath + "projects.png"}
-                        alt="#" aria-label="an image link leads to details about the art work"/>
-                </Link>
-              <button className='home-link-button'>Projects</button>
-            </div>
+            <Link to="/entertainment" className='home-img-links-container'>
+              <img className="home-img-links card-img-top" src={imagePath + "entertainment.png"}
+                alt="#" aria-label="an image link leads to details about the art work" />
+            </Link>
+            <Link to="/entertainment" className="btn main-btn mt-2">
+              <h5 className="">Media Center</h5>
+            </Link>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <p> We strive to create the world we want to live in. A world of color, and wonder! Where technology and art cross
-              <br />
-              Contact us today!
-            </p>
+        <div className="col">
+          <div className="card">
+            <Link to="/" className='home-img-links-container'>
+              <img className="home-img-links card-img-top" src={imagePath + "projects.png"}
+                alt="#" aria-label="an image link leads to details about the art work" />
+            </Link>
+            <Link to="#" className="btn main-btn mt-2">
+              <h5 className="">Projects</h5>
+            </Link>
           </div>
         </div>
       </div>
+      <div className="row">
+        <div className="col">
+          <p> We strive to create the world we want to live in. A world of color, and wonder! Where technology and art cross
+            <br />
+            Contact us today!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
