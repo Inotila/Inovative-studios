d01a9b1a0bc23ea22bb2c7cc5292eaf783036602
inoati1991@gmail.com <inoati1991@gmail.com>
2025-05-16 00:57:17 +0200

    Added variable for safety

--- backend/utils/contentful_connector/contentful.js
+++ backend/utils/contentful_connector/contentful.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const contentful = require('contentful');
 const client = contentful.createClient({
-    space: '47cj9gyw95uk',
-    accessToken: 'PqA6OvMoFavMCh-jdpT-O_jmLYisWv7yC3BkrcrsI6k',
+  space: process.env.CONTENTFUL_SPACE,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
 module.exports = {
